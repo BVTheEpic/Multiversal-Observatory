445ef48cfe31ce80996e0710373e01a6925a7985
BVTheEpic <bvtheepic@gmail.com>
2015-06-17 17:14:47 -0400

    Updated Form2.cs. Created a few Earths with placeholder descriptions.

---
+++ Multiversal Observatory- Version 2.0/Form2.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Multiversal_Observatory__Version_2._0
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.viewScreen = new System.Windows.Forms.Button();
+            this.earthDescription = new System.Windows.Forms.RichTextBox();
+            this.multiversalDesignate = new System.Windows.Forms.TextBox();
+            this.goTo = new System.Windows.Forms.Button();
+            this.multiversalOrrery = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // viewScreen
+            // 
+            this.viewScreen.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("viewScreen.BackgroundImage")));
+            this.viewScreen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.viewScreen.Enabled = false;
+            this.viewScreen.Location = new System.Drawing.Point(441, 12);
+            this.viewScreen.Name = "viewScreen";
+            this.viewScreen.Size = new System.Drawing.Size(429, 429);
+            this.viewScreen.TabIndex = 0;
+            this.viewScreen.UseVisualStyleBackColor = true;
+            this.viewScreen.Click += new System.EventHandler(this.viewScreen_Click);
+            // 
+            // earthDescription
+            // 
+            this.earthDescription.Location = new System.Drawing.Point(13, 198);
+            this.earthDescription.Name = "earthDescription";
+            this.earthDescription.ReadOnly = true;
+            this.earthDescription.Size = new System.Drawing.Size(422, 243);
+            this.earthDescription.TabIndex = 1;
+            this.earthDescription.Text = "";
+            // 
+            // multiversalDesignate
+            // 
+            this.multiversalDesignate.Location = new System.Drawing.Point(13, 13);
+            this.multiversalDesignate.Name = "multiversalDesignate";
+            this.multiversalDesignate.Size = new System.Drawing.Size(206, 22);
+            this.multiversalDesignate.TabIndex = 2;
+            // 
+            // goTo
+            // 
+            this.goTo.Location = new System.Drawing.Point(226, 11);
+            this.goTo.Name = "goTo";
+            this.goTo.Size = new System.Drawing.Size(209, 23);
+            this.goTo.TabIndex = 3;
+            this.goTo.Text = "Go to this Earth!";
+            this.goTo.UseVisualStyleBackColor = true;
+            this.goTo.Click += new System.EventHandler(this.goTo_Click);
+            // 
+            // multiversalOrrery
+            // 
+            this.multiversalOrrery.Location = new System.Drawing.Point(13, 55);
+            this.multiversalOrrery.Name = "multiversalOrrery";
+            this.multiversalOrrery.ReadOnly = true;
+            this.multiversalOrrery.Size = new System.Drawing.Size(422, 111);
+            this.multiversalOrrery.TabIndex = 4;
+            this.multiversalOrrery.Text = "";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(882, 453);
+            this.Controls.Add(this.multiversalOrrery);
+            this.Controls.Add(this.goTo);
+            this.Controls.Add(this.multiversalDesignate);
+            this.Controls.Add(this.earthDescription);
+            this.Controls.Add(this.viewScreen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form2";
+            this.Text = "Multiversal Observatory";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button viewScreen;
+        private System.Windows.Forms.RichTextBox earthDescription;
+        private System.Windows.Forms.TextBox multiversalDesignate;
+        private System.Windows.Forms.Button goTo;
+        private System.Windows.Forms.RichTextBox multiversalOrrery;
+    }
+}
--- Multiversal Observatory- Version 2.0/Form2.cs
+++ Multiversal Observatory- Version 2.0/Form2.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +12,94 @@ namespace Multiversal_Observatory__Version_2._0
 {
     public partial class Form2 : Form
     {
+        public class Earth
+        {
+            public string EarthName { get; set; } 
+            public string EarthDescription { get; set; }
+            public Earth(string earthName, string earthDescription)
+            {
+                EarthName = earthName;
+                EarthDescription = earthDescription;
+            }
+        }
+
+        List<Earth> multiverse = new List<Earth>();
+        
+        public void createMultiverse()
+        {
+            //note: Form2 displays pre-Flashpoint multiverse
+            //todo: "trim the fat" and remove any universes without substantial information
+
+            Earth earth1 = new Earth("Earth-1", "primary Earth; contains Justice League of America");
+            multiverse.Add(earth1);
+
+            Earth earth2 = new Earth("Earth-2", "contains Justice Society Infinity");
+            multiverse.Add(earth2);
+
+            Earth earth3 = new Earth("Earth-3", "contains Crime Society");
+            multiverse.Add(earth3);
+
+            Earth earth4 = new Earth("Earth-4", "Charleton Comics universe; contains Blue Beetle, Question, and Captain Atom");
+            multiverse.Add(earth4);
+
+            Earth earth5 = new Earth("Earth-5", "Fawcett Comics universe; contains Marvel Family");
+            multiverse.Add(earth5);
+
+            Earth earth6 = new Earth("Earth-6", "contains Marvel Comics analogues");
+            multiverse.Add(earth6);
+
+            Earth earth7 = new Earth("Earth-7", "contains Courtney Whitmore/Starwoman");
+            multiverse.Add(earth7);
+
+            Earth earth8 = new Earth("Earth-8", "contains Lord Havok and the Extremists");
+            multiverse.Add(earth8);
+
+            Earth earth9 = new Earth("Earth-9", "Tangent Comics universe");
+            multiverse.Add(earth9);
+
+            Earth earth10 = new Earth("Earth-10", "contains Freedom Fighters and Overman");
+            multiverse.Add(earth10);
+
+            Earth earth11 = new Earth("Earth-11", "reversed-gender universe");
+            multiverse.Add(earth11);
+
+            Earth earth12 = new Earth("Earth-12", "Batman Beyond universe");
+            multiverse.Add(earth12);
+
+            Earth earth13 = new Earth("Earth-13", "dark/arcane universe");
+            multiverse.Add(earth13);
+
+            Earth earth15 = new Earth("Earth-15", "utopian world destroyed by Superboy Prime");
+            multiverse.Add(earth15);
+
+            Earth earth16 = new Earth("Earth-16", "Young Justice universe");
+            multiverse.Add(earth16);
+
+            //todo: continue adding Earths
+            
+            multiversalOrrery.Text = "LIST OF EARTHS:";
+            for (int i = 0; i < multiverse.Count; i++)
+            {
+                multiversalOrrery.Text += "\n" + multiverse[i].EarthName;
+            }
+        }
+        
+        
         public Form2()
         {
             InitializeComponent();
+            createMultiverse();
         }
 
         private void goTo_Click(object sender, EventArgs e)
         {
-
+            for (int i = 0; i < multiverse.Count; i++)
+            {
+                if (multiverse[i].EarthName == multiversalDesignate.Text)
+                {
+                    earthDescription.Text = multiverse[i].EarthDescription;
+                }
+            }
         }
 
         private void viewScreen_Click(object sender, EventArgs e)
